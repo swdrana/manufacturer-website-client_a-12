94b04819eab374f636a665f253634cabbffad5dd
swdrana <tctr2s@gmail.com>
2022-05-28 01:03:07 +0600

    get blogs using axios

--- src/Pages/Blogs.js
+++ src/Pages/Blogs.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import Blog from '../components/Blog';
+import React from "react";
+import Blog from "../components/Blog";
+import useBlogs from "../hooks/useBlogs";
 
 const Blogs = () => {
-    return (
-        <div className=' p-5 lg:p-10 bg-base-300'>
-            <Blog/>
-            <Blog/>
-        </div>
-    );
+  const [blogs, setBlogs] = useBlogs();
+  return (
+    <div className=" p-5 lg:p-10 bg-base-300">
+      {blogs.map((blog) => {
+        return <Blog key={blog._id} blog={blog}/>
+      })}
+    </div>
+  );
 };
 
 export default Blogs;
--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const Blog = () => {
+const Blog = (props) => {
+    const {blogTitle, blog, image} = props.blog;
     return (
         <div className=' bg-base-100 mt-10 p-5  min-h-[250px] rounded-2xl'>
-            <h1 className='text-2xl'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo voluptates optio veniam est dolores dolore sapiente.</h1>
+            <h1 className='text-2xl'>{blogTitle}</h1>
             <div className="mt-5 relative">
-                <img src="https://i.ibb.co/jrR1KhS/image.png" className=' w-40 h-40 float-left mr-5' alt="" />
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga unde illo quod labore expedita minus aliquam, illum perferendis, accusantium sint libero amet ex deserunt corporis soluta nesciunt quae veritatis ipsam cupiditate vitae consequuntur iste quas explicabo? Possimus, magnam commodi nesciunt, nemo id illum recusandae facere ab ratione repudiandae nam reiciendis autem asperiores minus aspernatur. Nisi non suscipit magni necessitatibus sed ipsum ipsa debitis, quaerat facilis vel exercitationem hic voluptate expedita consectetur voluptates. Possimus veritatis dolores, placeat labore consequuntur beatae sunt similique corrupti optio commodi molestias mollitia nulla natus autem praesentium aperiam hic voluptatem, sit, deleniti consectetur ab velit id? Incidunt?</p>
+                <img src={image} className=' w-40 h-40 float-left mr-5' alt="" />
+                <p>{blog}</p>
             </div>
         </div>
     );
